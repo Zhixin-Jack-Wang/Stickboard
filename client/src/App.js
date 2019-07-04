289e88deaf57e57441292756909f3ac6473f8e6f
Zhixin Wang <harima.ky@gmail.com>
2019-07-04 15:55:46 -0400

    test api

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [user, setUser] = React.useState(null);
+  // const [user, setUser] = React.useState(null);
+     const [data, setData] = React.useState(null);
 
+  // React.useEffect(() => {
+  //   axios.get('/api?user=Zhixin-Jack-Wang')
+  //     .then(response => {
+  //       setUser(response.data);
+  //     });
+  // },[])
   React.useEffect(() => {
-    axios.get('/api?user=Zhixin-Jack-Wang')
+    axios.get('https://api.meetup.com/find/groups?zip=11211&radius=1&category=25&order=members')
       .then(response => {
         setUser(response.data);
+        console.log(response);
       });
   },[])
 
@@ -17,9 +25,12 @@ function App() {
     user && (
       <div className="App">
         <header className="App-header">
-          <img src={user.user.avatar_url} className="App-logo" alt="logo" />
+          {/* <img src={user.user.avatar_url} className="App-logo" alt="logo" />
           <p>
             {user.user.login}
+          </p> */}
+          <p>
+            {data}
           </p>
           <a
             className="App-link"
